069d63d271659de36867f8551780cac0d929a1c9
Yair Chen <yair.chen2000@gmail.com>
2021-04-02 00:38:10 +0300

    add blocs

--- App/lib/presentation/screens/formation_screen/widgets/formation_report_dialog.dart.dart
+++ App/lib/presentation/screens/formation_screen/widgets/formation_report_dialog.dart.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jedi/core/constants/strings.dart';
 import 'package:jedi/core/themes/app_theme.dart';
+import 'package:jedi/logic/cubit/report_type/report_type_cubit.dart';
+import 'package:jedi/logic/cubit/student_report/student_report_cubit.dart';
+import 'package:jedi/logic/entities/report_type.dart';
 import 'package:jedi/logic/entities/student.dart';
+import 'package:jedi/logic/entities/student_report.dart';
 
 class FormationReportDialog extends StatelessWidget {
   final StudentEntity student;
@@ -9,6 +15,50 @@ class FormationReportDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final reportTypesState = context.watch<ReportTypeCubit>().state;
+    final studentReportState = context.watch<StudentReportCubit>().state;
+
+    if (studentReportState is StudentReportSaveSuccess) {
+      Navigator.of(context).pop();
+    }
+
+    if (reportTypesState is ReportTypeFetchSuccess &&
+        studentReportState is StudentReportTodayFetchSuccess) {
+      return _fetchReportTypesSuccess(
+        context,
+        reportTypesState,
+        studentReportState,
+      );
+    } else {
+      return _fetchReportTypesInProgress(context);
+    }
+  }
+
+  Widget _fetchReportTypesInProgress(BuildContext context) {
+    final circularProgressIndicatorSize =
+        MediaQuery.of(context).size.height * 0.1;
+
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      content: Container(
+        height: circularProgressIndicatorSize,
+        child: Center(
+          child: SizedBox(
+            height: circularProgressIndicatorSize,
+            width: circularProgressIndicatorSize,
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _fetchReportTypesSuccess(
+    BuildContext context,
+    ReportTypeFetchSuccess reportTypeState,
+    StudentReportTodayFetchSuccess studentReportState,
+  ) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: AlertDialog(
@@ -29,21 +79,27 @@ class FormationReportDialog extends StatelessWidget {
           children: [
             Wrap(
               direction: Axis.horizontal,
-              children: _buildFormationReportTypes(),
+              children: _buildFormationReportTypesCheckboxes(
+                context,
+                reportTypeState,
+                studentReportState,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10),
               child: MaterialButton(
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
                   child: Text(
-                    'שמור דיווח',
+                    Strings.saveReport,
                     style: Theme.of(context).textTheme.subtitle1,
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  context.read<StudentReportCubit>().saveStudentReports();
+                },
                 color: AppTheme.success_color,
               ),
             )
@@ -53,27 +109,38 @@ class FormationReportDialog extends StatelessWidget {
     );
   }
 
-  List<Widget> _buildFormationReportTypes() {
-    final reportTypes = [
-      'גלח"ץ',
-      'כומתה',
-      'שיער',
-      'תגיות',
-      'גילוח',
-      'אחר',
-    ];
+  List<Widget> _buildFormationReportTypesCheckboxes(
+    BuildContext context,
+    ReportTypeFetchSuccess reportTypeState,
+    StudentReportTodayFetchSuccess studentReportState,
+  ) {
+    final studentReportCubit = context.read<StudentReportCubit>();
 
-    return reportTypes
+    return reportTypeState.reportTypes
         .map((type) => Row(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Checkbox(
                   activeColor: AppTheme.success_color,
-                  value: true,
-                  onChanged: (value) {},
+                  value: studentReportState
+                      .doesHaveReportFromType(type.reportTypeId),
+                  onChanged: (value) {
+                    if (value == false) {
+                      studentReportCubit
+                          .removeReportToStudent(type.reportTypeId);
+                    } else {
+                      studentReportCubit.addReportToStudent(
+                        StudentReportEntity(
+                          studentId: studentReportState.studentId,
+                          reportType: type,
+                          dateReported: DateTime.now(),
+                        ),
+                      );
+                    }
+                  },
                 ),
                 Text(
-                  type,
+                  type.name,
                   style: TextStyle(color: Colors.black),
                 ),
               ],
