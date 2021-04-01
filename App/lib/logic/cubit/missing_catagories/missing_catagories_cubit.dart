51837d2090b670dfaa2a5eaa7423139a80ff5156
Yair Chen <yair.chen2000@gmail.com>
2021-04-02 00:34:49 +0300

    delete unnecessary files

--- App/lib/logic/cubit/missing_catagories/missing_catagories_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
-
-part 'missing_catagories_state.dart';
-
-class MissingCatagoriesCubit extends Cubit<MissingCatagoriesState> {
-  MissingCatagoriesCubit() : super(MissingCatagoriesCubit());
-}
--- App/lib/logic/cubit/student_report_cubit/student_report_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'student_report_state.dart';
-
-class StudentReportCubit extends Cubit<StudentReportState> {
-  StudentReportCubit() : super(StudentReportInitial());
-}
