a25a1cd1453fce2b123051baf210d41d4a0a40f5
Yair Chen <yair.chen2000@gmail.com>
2021-04-03 14:11:17 +0300

    add watch for missing student state

--- App/lib/presentation/screens/formation_screen/formation_screen.dart
+++ App/lib/presentation/screens/formation_screen/formation_screen.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:jedi/data/models/report_type.dart';
+import 'package:jedi/core/themes/app_theme.dart';
 import 'package:jedi/logic/bloc/students_bloc/students_bloc.dart';
 import 'package:jedi/logic/cubit/report_type/report_type_cubit.dart';
+import 'package:jedi/logic/cubit/student_missing/student_missing_cubit.dart';
 import 'package:jedi/presentation/screens/formation_screen/widgets/formation_body.dart';
 
 class FormationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    context.read<StudentMissingCubit>().getMissingStudentsToday();
+    context.read<ReportTypeCubit>().getReportTypes();
+
     return Scaffold(
       body: BlocBuilder<StudentsBloc, StudentsState>(
         buildWhen: (previous, current) => !(previous is StudentFetchSuccess &&
             current is StudentFetchSuccess),
-        builder: (context, state) {
-          if (state is StudentFetchInProgress) {
-            return Center(child: CircularProgressIndicator());
+        builder: (context, studentsState) {
+          final missingStudentsState =
+              context.watch<StudentMissingCubit>().state;
+
+          if (studentsState is StudentFetchInProgress ||
+              missingStudentsState is StudentMissingTodayFetchInProgress) {
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Theme.of(context).success),
+              ),
+            );
+          } else {
+            return FormationBody();
           }
-
-          context.read<ReportTypeCubit>().getReportTypes();
-
-          return FormationBody();
         },
       ),
     );
